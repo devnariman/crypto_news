a4e635462fd9a5b73d153295e02f58cc3dfb4bdd
devnariman <dev.narimanmirbod@gmail.com>
2025-08-12 17:23:18 +0330

    5:23

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from webNEWs import web_news
+from run_just_bot import dev_pirate_crypto_nonenews
 import json
 from Bot_tel import dev_pirate_crypto
 
 
-num = int(input("Enter the number of news items to fetch: "))
-a = web_news("https://www.investing.com/crypto/bitcoin")
-res = a.run_BTC(number=num)
-print("Telegram Bot Runed ! ")
-b = dev_pirate_crypto('8492375819:AAEr8cbQEIMqacxzW5JS6mSjEGGAWH_ReYk')
-b.run()
+print("how mod bot run ?")
+mod = input("1 - with news\n2 - without news\nenter mod: ")
+if mod == "1":
+    num = int(input("Enter the number of news items to fetch: "))
+    a = web_news("https://www.investing.com/crypto/bitcoin")
+    res = a.run_BTC(number=num)
+    print("Telegram Bot Runed ! ")
+    b = dev_pirate_crypto('8492375819:AAEr8cbQEIMqacxzW5JS6mSjEGGAWH_ReYk')
+    b.run()
+elif mod == "2":
+    print("Telegram Bot Runed ! ")
+    b = dev_pirate_crypto_nonenews('8492375819:AAEr8cbQEIMqacxzW5JS6mSjEGGAWH_ReYk')
+    b.run()
---
+++ run_just_bot.py
@@ -0,0 +1,17 @@
+# Bot_tel.py
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, Application, ContextTypes
+from dev_func import start, handle_message, send_news_to_all   # ← اینو پایین می‌نویسم
+import asyncio
+
+class dev_pirate_crypto_nonenews:
+    def __init__(self, Token: str):
+        self.app: Application = (
+            ApplicationBuilder()
+            .token(Token)
+            .build()
+        )
+        self.app.add_handler(CommandHandler("start", start))
+        self.app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
+    def run(self):
+        self.app.run_polling()
